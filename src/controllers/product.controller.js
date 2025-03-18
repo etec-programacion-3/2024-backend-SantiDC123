a9289bac3baf300b282b8cb19feb11e64d918a82
santiago di cesare <s.dicesare@alumno.etec.um.edu.ar>
2025-03-18 00:09:00 -0300

    Desarrollo de ruta y función para listar el detalle del producto

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,12 +1,12 @@
 import Product from "../models/product.model.js";
 
 
-export const crearProducto = async (req,res) => {
-    const {titulo,descripcion,precio,stock,categoria,portada} = req.body;
+export const crearProducto = async (req, res) => {
+    const { titulo, descripcion, precio, stock, categoria, portada } = req.body;
 
-    if (titulo == '' || descripcion == '' || precio == '' || stock == '' || categoria == '' || portada == ''  ) {
+    if (titulo == '' || descripcion == '' || precio == '' || stock == '' || categoria == '' || portada == '') {
         res.status(400).json({ message: 'Error: todos los campos deben ser completados.' })
-    }else{
+    } else {
         try {
             const nuevoProducto = new Product({
                 titulo,
@@ -26,11 +26,22 @@ export const crearProducto = async (req,res) => {
 }
 
 
-export const listarProductos = async (req,res) => {
+export const listarProductos = async (req, res) => {
     try {
         const listadoProductos = await Product.find();
         res.json(listadoProductos)
     } catch (error) {
-        return res.status(500).json({ message: "Ha ocurrido un error al intentar acceder al listado de productos."})
+        return res.status(500).json({ message: "Ha ocurrido un error al intentar acceder al listado de productos." })
+    }
+}
+
+
+export const listarDetalleProducto = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const producto = await Product.findById(id);
+        res.json(producto)
+    } catch (error) {
+        return res.status(500).json({ message: "Ha ocurrido un error al intentar acceder al detalle del producto." })
     }
 }
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { crearProducto, listarProductos } from "../controllers/product.controller.js";
+import { crearProducto, listarDetalleProducto, listarProductos } from "../controllers/product.controller.js";
 
 const router = Router();
 
 router.post('/product', crearProducto)
 
 router.get('/product', listarProductos)
+router.get('/product/detail/:id', listarDetalleProducto)
 
 export default router;
